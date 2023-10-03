c9b165f19799682265e3b9ab2b3f221ce1561c2b
pijush karmakar <pijushkarmakar45@gmail.com>
2023-10-03 20:27:46 +0600

    Assignment Module04

---
+++ product.php
@@ -0,0 +1,52 @@
+<?php
+
+class Product {
+    // TODO: Add properties
+    public int $id;
+    public string $name;
+    public float $price;
+    
+    public function __construct($id, $name, $price) {
+        // TODO: Initialize properties
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+
+    // TODO: Add getFormattedPrice method
+    public function getFormattedPrice(){
+        // return number_format($this->price, 2);
+        return sprintf("%.2f",$this->price);
+    }
+
+    // TODO: Add showDetails method
+    public function showDetails(){
+        echo "Product Details:\n-ID: {$this->id}\n-Name: {$this->name}\n-Price: $"."{$this->getFormattedPrice()}";
+    }
+}
+
+
+// Test the Product class
+$product = new Product(1, 'T-shirt', 19.993412);
+$product->showDetails();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
